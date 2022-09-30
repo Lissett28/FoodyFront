0e6bce272ad67ca6d505073c4c7d1552a1f6baeb
Junhe-Chen-9 <contact@junhechen.com>
2022-09-29 20:28:47 -0700

    creating file

---
+++ create-schema.sql
@@ -0,0 +1,6 @@
+-- -----------------------------------------------------
+-- Schema foody-app
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `foody-app`;
+CREATE SCHEMA `foody-app`;
+USE `foody-app` ;
