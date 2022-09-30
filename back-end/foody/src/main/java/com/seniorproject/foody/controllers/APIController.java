3da9b91bdc5ef1dc79c55927c02fa242642c6e39
junhe chen <contact@junhechen.com>
2022-09-30 11:24:48 -0700

    api call to yelp

--- back-end/foody/src/main/java/com/seniorproject/foody/controllers/APIController.java
+++ back-end/foody/src/main/java/com/seniorproject/foody/controllers/APIController.java
@@ -5,17 +5,43 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @CrossOrigin("http://localhost:3300")//react is at 3300
 @RestController
 @RequestMapping("/api/locate")
 public class APIController {
 
     @GetMapping(value="find/{address}&{radius}",produces = "application/json")
-    public String findByAddressAndRadius(@PathVariable("address") String address,
+    public List<Object> findByAddressAndRadius(@PathVariable("address") String address,
                                                           @PathVariable("radius") String radius){
 
 
-        String url="https://api.yelp.com/v3";
-        return address + "and" + radius;
+
+
+        // api constants
+        String api_host = "https://api.yelp.com";
+        String search_path = "/v3/businesses/search";
+        String bussinese_path = "/v3/businesses/";
+
+        // search terms
+        String term = "dinner";
+        String location = "Los+Angeles,+CA";
+        int search_limits = 3; // limit to display 3 items
+
+        return search(api_host,bussinese_path,term,location);
+    }
+    private List<Object> request(String host, String path, String url){
+        url = host.concat(path);
+        RestTemplate restTemplate = new RestTemplate();
+        Object[] response = restTemplate.getForObject(url,Object[].class);
+        return Arrays.asList(response);
+    }
+
+    private List<Object> search(String host,String path, String term,String locations){
+        String search_url = "term" + term + "location" + locations + "limit" + 3;
+        return request(host,path,search_url);
     }
 }
