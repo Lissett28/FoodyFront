659649924d741165c4585b1ff3bd0096147d49ea
junhe chen <contact@junhechen.com>
2022-10-05 19:53:17 -0700

    got rid of the useless stuff

--- back-end/foody/src/main/java/com/seniorproject/foody/entities/Business.java
+++ back-end/foody/src/main/java/com/seniorproject/foody/entities/Business.java
@@ -1,10 +1,9 @@
 package com.seniorproject.foody.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
+
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
